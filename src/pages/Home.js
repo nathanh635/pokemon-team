9a9b58330c55b0379b0cf67f22fb51a1f5df06b3
Nathan <nathanh635@gmail.com>
2023-02-03 14:06:34 -0700

    one card working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,48 @@
 import Card from 'react-bootstrap/Card';
+import React, { useState, useEffect } from 'react';
+
 import PokemonCard from './PokemonCard';
 
 
 const Home = () => {
 
-
-  let ids = [1, 2, 3,4, 5, 6]
-    // for(let i = 0; i<6;i++){
-    // ids[i] = Math.floor(Math.random() * 151) + 1;}
-
-    console.log(ids);
+  const [pokemon, setPokemon] = useState([]);
 
 
+    useEffect(() => {
+      console.log("useEffect");
+      let ids = [];
+      for(let i = 0; i<6;i++){
+      ids[i] = Math.floor(Math.random() * 151) + 1;}
+
+      for(let i = 0; i<6; i++){
+      fetch(`https://pokeapi.co/api/v2/pokemon/${ids[i]}/`)
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+          setPokemon([...pokemon, data]);
+    })      .catch((err) => {
+      console.log(err.message);
+   });}}, []);
+
+   console.log(pokemon);
 
   return (
     <>
     <h1>What's your Pokemon Team?</h1>
 
 
-      {ids.map((result) => (
-        <PokemonCard id={result}/>
+      {pokemon.map((poke) => (
+            <Card style={{ width: '18rem' }}>
+            { <Card.Img variant="top" src={poke.sprites.front_default} /> }
+            <Card.Body>
+              <Card.Title>{poke.name}</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
       ))}
 
 
--- src/pages/PokemonCard.js
+++ src/pages/PokemonCard.js
@@ -1,27 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
 
 
-import axios from 'axios';
+
 import Poke from './Poke';
 
 const PokemonCard = ({id}) => {
 
   const [pokemon, setPokemon] = useState();
 
-  const pokemonSearch = async (id) => {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    setPokemon(response.data);
-    console.log(pokemon);
-  };
 
   useEffect(() => {
-    console.log("useEffect")
-    pokemonSearch(id);
-  }, []);
+    console.log("useEffect");
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data);
+        setPokemon(data);
+  })      .catch((err) => {
+    console.log(err.message);
+ });}, []);
 
 
-  return (<div><p>{id}</p>
-  {/* <Poke pokemon = {pokemon}/> */}
+
+
+  return (<div>
+    <Card style={{ width: '18rem' }}>
+    { <Card.Img variant="top" src={pokemon.sprites.front_default} /> }
+    <Card.Body>
+      <Card.Title>{pokemon.name}</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
   </div>
 
 
